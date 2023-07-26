27ccf27f1029504f41eb3cdc3eaa3d7f428f9d31
athibau2 <a.thibs98@gmail.com>
2023-07-26 01:15:34 -0600

    added hidden input

--- components/Contact.vue
+++ components/Contact.vue
@@ -18,6 +18,7 @@
                     netlify
                     :style="windowWidth < 700 ? {'width': '95%'} : null"
                 >
+                    <input type="hidden" name="form-name" value="contact-form">
                     <span class="header">Send Me a Message!</span>
                     <v-row class="form-row" align="center" justify="center">
                         <input class="form-row-field"
